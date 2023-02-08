97169ef04cb2cf3751fc92017caff3a7cf0e0e8a
abelEsperon <abel21ge@gmail,com>
2023-02-08 02:13:49 -0600

    Initial Commit

---
+++ FirebaseLink/AuthViewController.swift
@@ -0,0 +1,70 @@
+//
+//  AuthViewController.swift
+//  FirebaseLink
+//
+//  Created by Abel Gonzalez on 02/02/23.
+//
+
+import UIKit
+import FirebaseAnalytics
+import FirebaseAuth
+
+class AuthViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwdTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Authentication"
+        // Do any additional setup after loading the view.
+        Analytics.logEvent("InitScreen", parameters: ["message":"Integracion de Firebase"])
+    }
+    
+    @IBAction func signUpButtonAction(_ sender: Any) {
+        
+        if let email = emailTextField.text, let password = passwdTextField.text {
+            Auth.auth() .createUser(withEmail: email, password: password) {
+                (result, error) in
+
+                if let result = result, error == nil {
+
+                    self.navigationController?
+                        .pushViewController(HomeViewController(email:
+                                                                result.user .email!, provider: .basic), animated: true)
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: "Se ha producido un error registrando el usuario", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title:"Aceptar", style: .default))
+                    self.present (alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func logInButtonAction(_ sender: Any) {
+        
+        if let email = emailTextField.text, let password = passwdTextField.text {
+            Auth.auth() .signIn(withEmail: email, password: password) {
+                (result, error) in
+
+                if let result = result, error == nil {
+
+                    self.navigationController?
+                        .pushViewController(HomeViewController(email:
+                                                                result.user .email!, provider: .basic), animated: true)
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: "Se ha producido un error registrando el usuario", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title:"Aceptar", style: .default))
+                    self.present (alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+
+}
+
---
+++ FirebaseLink/HomeViewController.swift
@@ -0,0 +1,57 @@
+//
+//  HomeViewController.swift
+//  FirebaseLink
+//
+//  Created by Abel Gonzalez on 02/02/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+enum ProviderType: String {
+    case basic
+}
+
+class HomeViewController: UIViewController{
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var providerLabel: UILabel!
+    @IBOutlet weak var closeSesionButton: UIButton!
+    
+    private let email: String
+    private let provider: ProviderType
+
+    init (email: String, provider: ProviderType) {
+        self.email = email
+        self.provider = provider
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Inicio"
+        
+        emailLabel.text = email
+        providerLabel.text = provider.rawValue
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func closeSesionButtonAction(_ sender: Any) {
+        switch provider {
+        case .basic:
+            do {
+                try Auth.auth().signOut()
+                navigationController?.popViewController(animated: true)
+            } catch {
+             //Se ha producido un error
+            }
+        }
+    }
+
+}
